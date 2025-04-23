26ce8dcd5965ffdd45905150faf6a3085b0f8c60
MariaOs02 <maria.ostosp@udea.edu.co>
2025-04-23 16:47:21 -0500

    json y csv

--- procesadorArchivos.py
+++ procesadorArchivos.py
@@ -9,6 +9,7 @@ def leer_archivos(directorio):
         ruta = os.path.join(directorio, archivo)
 
         try:
+            
             if archivo.endswith('.txt'):
                 with open(ruta, 'r', encoding='utf-8') as f:
                     lineas = [linea.strip() for linea in f.readlines()]
@@ -44,14 +45,63 @@ def leer_archivos(directorio):
                         'tipo_archivo': 'txt',
                         'datos': paciente
                     })
-
-            '''
             elif archivo.endswith('.json'):
+                with open(ruta, 'r', encoding='utf-8') as f:
+                    datos = json.load(f)
+                    paciente = {}
+                    for d in datos:
+                        d['ID'] = d.get('id')
+                        paciente['ID'] = d.get('id')
+                        paciente['Nombres'] = d.get('Pname')
+                        paciente['Apellidos'] = d.get('Plastname')
+                        paciente['Genero'] = d.get('gender')
+                        paciente['Edad'] = d.get('age')
+                        test = d.get('test')
+                        paciente['HDL'] = test['HDL']
+                        paciente['LDL'] = test['LDL']
+                        paciente['TRIG'] = test['TRIG']
+                        #pacientes.append({'archivo': archivo, 'datos': d})  
+
+                    pacientes.append({
+                        'archivo': archivo,
+                        'tipo_archivo': 'txt',
+                        'datos': paciente
+                    })
+            
+            elif archivo.endswith('.csv'):
+                with open(ruta, 'r', encoding='utf-8') as f:
+                    reader = csv.DictReader(f, delimiter=';')
+                    reader.fieldnames = [campo.strip() for campo in reader.fieldnames]
+                    paciente = {}
+                    for row in reader:
+                        paciente['ID'] = row.get('id')
+                        print(row.get('id'))
+                        paciente['Nombres'] = row.get('name')
+                        paciente['Apellidos'] = row.get('lastname')
+                        paciente['Genero'] = row.get('gender')
+                        paciente['Edad'] = row.get('age')
+                        paciente['TP'] = row.get('test_tp')
+                        paciente['TPT'] = row.get('test_ptt')
+                        paciente['FIB'] = row.get('test_fib')
+
+                    pacientes.append({
+                        'archivo': archivo,
+                        'tipo_archivo': 'txt',
+                        'datos': paciente
+                    })
+                        
+
+                    '''
+
+            
+            if archivo.endswith('.json'):
                 with open(ruta, 'r', encoding='utf-8') as f:
                     datos = json.load(f)
                     for d in datos:
                         d['ID'] = d.get('id')
                         pacientes.append({'archivo': archivo, 'datos': d})  
+  
+                    
 
             elif archivo.endswith('.csv'):
                 with open(ruta, 'r', encoding='utf-8') as f:
